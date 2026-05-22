adbb7313c2fa1e8e5c0e0062348c54edc995ca98
ckdwlsv <ckdwlsv@naver.com>
2026-05-22 11:24:59 +0900

    fix : 다시

--- src/main/java/com/my/ox_quiz/member/entity/RoleType.java
+++
@@ -1,6 +0,0 @@
-package com.my.ox_quiz.member.entity;
-
-public enum RoleType {
-    ADMIN,
-    USER
-}
--- src/main/java/com/my/ox_quiz/quiz/repository/QuizRepository.java
+++
@@ -1,8 +0,0 @@
-package com.my.ox_quiz.quiz.repository;
-
-
-import com.my.ox_quiz.quiz.entity.Quiz;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface QuizRepository extends JpaRepository<Quiz,Long> {
-}
